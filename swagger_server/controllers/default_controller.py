cd07fda1005390afda5b6f4a6abf689179234950
BalandinIlia <ilia.balandin@student.uva.nl>
2025-03-06 16:40:09 +0100

    Update default_controller.py

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -17,7 +17,8 @@ def calc():
     global i
     i = i + 1
     response = s3.put_object(Bucket = "arn:aws:s3:::temporalal", Key = "python_message.txt", Body = "Hello, S3!".encode("utf-8"))
-    return {"status": print(response)}, 200
+    str = print(response)
+    return {"status": str}, 200
 
 def healthcheck():  # noqa: E501
     """healthcheck
