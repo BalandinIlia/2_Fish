c40c11bb736dde8ead79a07bfd1224e32e653520
BalandinIlia <ilia.balandin@student.uva.nl>
2025-03-12 14:33:08 +0100

    Update default_controller.py

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -4,22 +4,22 @@ import boto3
 import sys
 
 # Create an S3 client
-s3 = boto3.client("s3")
+#s3 = boto3.client("s3")
 
 # Specify your bucket and object
-bucket_name = "temporalal"
-object_key = "python_message.txt"
+#bucket_name = "temporalal"
+#object_key = "python_message.txt"
 
 from swagger_server import util
 
 i = 0
 
 def calc():
-    print("This is an error message", file=sys.stderr)
-    global i
-    i = i + 1
-    response = s3.put_object(Bucket = bucket_name, Key = object_key, Body = "Hello, S3!".encode("utf-8"))
-    str = print(response)
+    #print("This is an error message", file=sys.stderr)
+    #global i
+    #i = i + 1
+    #response = s3.put_object(Bucket = bucket_name, Key = object_key, Body = "Hello, S3!".encode("utf-8"))
+    #str = print(response)
     return {"status": str}, 200
 
 def healthcheck():  # noqa: E501
