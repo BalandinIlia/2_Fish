6bc778d2aca774988f51eac36c1b966fad7ddb17
BalandinIlia <ilia.balandin@student.uva.nl>
2025-03-12 14:21:59 +0100

    Update default_controller.py

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -1,6 +1,7 @@
 import connexion
 import six
 import boto3
+import sys
 
 # Create an S3 client
 s3 = boto3.client("s3")
@@ -14,6 +15,7 @@ from swagger_server import util
 i = 0
 
 def calc():
+    print("This is an error message", file=sys.stderr)
     global i
     i = i + 1
     response = s3.put_object(Bucket = bucket_name, Key = object_key, Body = "Hello, S3!".encode("utf-8"))
